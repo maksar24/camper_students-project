676efbaf09cc0fe28280bc6740342181b35b1719
maksar24 <74876375+maksar24@users.noreply.github.com>
2024-05-15 19:50:24 +0000

    Deploying to gh-pages from @ maksar24/camper_students-project@e26b93ff87b80845cdcbe87588bffb68c7f3664f 🚀

---
+++ static/js/368.6cb7cfdb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[368],{368:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"HomePage"})}}}]);
+//# sourceMappingURL=368.6cb7cfdb.chunk.js.map
