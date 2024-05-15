e26b93ff87b80845cdcbe87588bffb68c7f3664f
Leskovets Maksym <leskovecmaksim@gmail.com>
2024-05-15 21:49:41 +0200

    prepared structure of project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('../pages/HomePages/HomePage'));
+const FavoritePage = lazy(() => import('../pages/FavoritesPages/FavoritePage'));
+const CatalogPage = lazy(() => import('../pages/CatalogPages/CatalogPage'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes path="/">
+      <Route index element={<HomePage />} />
+      <Route path="/catalog" element={<CatalogPage />} />
+      <Route path="/favorites" element={<FavoritePage />} />
+    </Routes>
   );
 };
